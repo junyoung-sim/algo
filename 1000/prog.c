e8a61331ff397a3c74c2a016c23c7e7dbc491017
junyoung-sim <jysim.ny@gmail.com>
2024-12-20 16:05:24 -0500

    2024-12-20

---
+++ 1000/prog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int A;
+  int B;
+
+  scanf("%d %d", &A, &B);
+
+  printf("%d", A+B);
+
+  return 0;
+}
---
+++ 10998/prog.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  int A;
+  int B;
+
+  std::cin >> A;
+  std::cin >> B;
+
+  std::cout << A*B;
+
+  return 0;
+}
